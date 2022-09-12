a832dc42251084574bd9f8304743dce589dab7c1
daniilSimakhin <danil.110@yandex.ru>
2022-09-12 15:32:59 +0700

    Test

--- RickAndMorty/SceneDelegate.swift
+++ RickAndMorty/SceneDelegate.swift
@@ -21,7 +21,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         let window = UIWindow(windowScene: windowScene)
         window.rootViewController = ViewController()
         window.makeKeyAndVisible()
-        self.window = window
+        self.window = window 
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
