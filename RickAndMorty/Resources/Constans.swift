ba0be62fe0816e5122ee67b3d8698da0e28c9e47
daniilSimakhin <danil.110@yandex.ru>
2022-09-12 15:51:51 +0700

    Create a Constans file

--- RickAndMorty/Resources/Constans.swift
+++ RickAndMorty/Resources/Constans.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+import UIKit
+
+struct Constans {
+    struct Colors {
+        struct Tabbar {
+            static let unselectedItemTint = UIColor(named: "unselectedItemTint")
+            static let tabBarBackground = UIColor(named: "tabBarBackground")
+            static let tabBarTint = UIColor(named: "tabBarTint")
+        }
+    }
+
+    struct Text {
+        struct Tabbar {
+            static let locations = "Locations"
+            static let characters = "Characters"
+            static let episodes = "Episodes"
+        }
+    }
+
+    struct Images {
+        struct Tabbar {
+            static let locations = UIImage(systemName: "globe.americas.fill")
+            static let characters = UIImage(systemName: "person.2.circle.fill")
+            static let episodes = UIImage(systemName: "play.circle.fill")
+        }
+    }
+}
